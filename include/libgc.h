f635a36f90bbcbaeb9833367371410d7f5f9bae2
Jon <jon_larrea@hotmail.com>
2020-02-14 00:56:24 +0000

    Double free detection done. TODO: Remove double linked list

--- include/libgc.h
+++ include/libgc.h
@@ -7,9 +7,15 @@
 #undef GC_malloc
 #endif
 
-#define GC_malloc(...) GC_malloc_(__FILE__, __FUNCTION__, __LINE__, __VA_ARGS__);
+#ifdef GC_free
+#undef GC_free
+#endif
 
+#define GC_malloc(...) GC_malloc_(__FILE__, __FUNCTION__, __LINE__, __VA_ARGS__);
+#define GC_free(...) GC_free_(__FILE__, __FUNCTION__, __LINE__, __VA_ARGS__);
+
+void GC_enable_logs(void);
 void * GC_malloc_(const char * filename, const char * function, int line, size_t size);
-void GC_free(void * chunk);
+void GC_free_ (const char * filename, const char * function, int line, void * chunk);
 
 #endif
--- source/libgc.c
+++ source/libgc.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-#define RED   "\x1B[31m"
+
 #define RESET "\x1B[0m"
+#define RED     "\033[1m\033[31m"
+#define GREEN   "\033[1m\033[32m"
+#define YELLOW  "\033[1m\033[33m"
+
+#define TRUE 	1
+#define FALSE 	0
 
 typedef struct _Node Node;
 
@@ -10,16 +17,27 @@ struct _Node
 {
 	Node * prev;
 	void * ptr;
+	uint8_t free;
 	const char * filename;
 	const char * function;
 	int line;
 	Node * next;
 };
 
-void GC_collect (void) __attribute__ ((destructor));
+/* Flag that enables screen logs */
+uint8_t LOGS_FLAG = FALSE;
+
 
 Node * first = NULL;
 Node * last = NULL;
+
+/* Run collector after main function */
+void GC_collect (void) __attribute__ ((destructor));
+
+void GC_enable_logs(void)
+{
+	LOGS_FLAG = TRUE;
+}
 
 void * GC_malloc_ (const char * filename, const char * function, int line, size_t size)
 {
@@ -45,6 +63,7 @@ void * GC_malloc_ (const char * filename, const char * function, int line, size_
 		new->filename = filename;
 		new->function = function;
 		new->line = line;
+		new->free = FALSE;
 		return new->ptr;
 	}
 
@@ -56,11 +75,17 @@ void * GC_malloc_ (const char * filename, const char * function, int line, size_
 	new->filename = filename;
 	new->function = function;
 	new->line = line;
+	new->free = FALSE;
 	return new->ptr;
 }
 
-void GC_free (void * chunk)
+void GC_free_ (const char * filename, const char * function, int line, void * chunk)
 {
+	/* Remove unused warnings */
+	(void) filename;
+	(void) function;
+	(void) line;
+
 	Node * aux;
 	if(chunk == NULL)
 		return;
@@ -78,54 +103,65 @@ void GC_free (void * chunk)
 	if(aux->ptr != chunk)
 		return;
 
+	/* Double free check */
+	if(aux->free == TRUE)
+	{
+		if(LOGS_FLAG == TRUE)
+			printf("%s%s:%d%s: Double free in '%s'.\n",RED, filename, line, RESET, function);
+		return;
+	}
+
 	/*Free the only allocated chunk*/
 	if(aux->prev == NULL && aux->next ==NULL)
 	{
 		free(aux->ptr);
-		free(aux);
-		first = last = NULL;
+		aux->free = TRUE;
 	}
 	else if(aux->prev == NULL) /*Free the first node*/
 	{
-		first = aux->next;
-		first->prev = NULL;
 		free(aux->ptr);
-		free(aux);
+		aux->free = TRUE;
 	}
 	else if(aux->next == NULL) /*Free the last node*/
 	{
-		last = aux->prev;
-		last->next = NULL;
 		free(aux->ptr);
-		free(aux);
+		aux->free = TRUE;
 	}
 	else /*Free an intermediate chunk*/
 	{
-		(aux->prev)->next = aux->next;
-		(aux->next)->prev = aux->prev;
 		free(aux->ptr);
-		free(aux);
+		aux->free = TRUE;
 	}
 	return;
 }
 
 void printNode(Node * node)
 {
-	printf("%s%s:%d%s: Unfreed memory chunck in '%s'.\n", RED, node->filename, node->line, RESET, node->function);
+	printf("%s%s:%d%s: Unfreed memory chunck in '%s'.\n", YELLOW, node->filename, node->line, RESET, node->function);
 }
 
 void GC_collect (void)  
 { 
-    Node * node, * aux;
-    node = first;
-    /*Free all the allocated chunks*/
-    while(node != NULL)
-    {
-    	printNode(node);
-    	aux = node->next;
-    	free(node->ptr);
-    	free(node);
-    	node = aux;
-    }
-    return;
+	Node * node, * aux;
+	node = first;
+	uint8_t flag = TRUE;
+
+	/*Free all the allocated chunks*/
+	while(node != NULL)
+	{
+		aux = node->next;
+		if(node->free == FALSE)
+		{
+			if(LOGS_FLAG == TRUE)
+				printNode(node);
+			free(node->ptr);
+			flag = FALSE;
+		}
+		free(node);
+		node = aux;
+	}
+	if(LOGS_FLAG == TRUE && flag == TRUE)
+		printf("%sOK%s: All memory chuncks freed.\n", GREEN, RESET);
+
+	return;
 } 
